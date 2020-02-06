6d7d4126457627fcfe5ddaac8dc5045d018c030b
Shu Isaka - Home <shu.h7.research@gmail.com>
2020-02-06 14:06:43 +0900

    HtmlファイルをDjango経由で表示

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'resources/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
